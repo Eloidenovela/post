897b5683fbc1ab61ed33ffa435f76192d453763c
Eloidenovela <eloide.novela@outlook.com>
2024-08-07 05:52:30 +0200

    feat: added admin controller, to he grant other users editor privileges

---
+++ post/view/index.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+// #include "webui.h"
+#include "webui.hpp"
+
+namespace view {
+    inline std::string index() {
+        return R"(
+
+        )";
+    }
+
+    // webui::window win;
+    // win.webui_show(10, index());
+}
